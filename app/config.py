613a61e8e6176053ed3b07fe484f5617d9be4ab1
Andrey Belskiy <diksandab@gmail.com>
2021-02-27 02:53:51 +0600

    Created models. Updated templates and views

--- app/config.py
+++ app/config.py
@@ -1,2 +1,4 @@
 class Configuration():
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://sblog:sblogDBpasswd@localhost/sblogdb'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, render_template
 
+from models import Post
+
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
 @posts.route('/')
 def index():
-    return render_template('posts/index.html')
+    posts = Post.query.all()
+    return render_template('posts/index.html', posts=posts)
