8cec3761a58f7c889fed0feafecf959bbb2aaf4d
Andrey Belskiy <diksandab@gmail.com>
2021-03-06 23:43:59 +0600

    Added admin panel. Updated requirements

--- app/app.py
+++ app/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
-from config import Configuration
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
+from config import Configuration
 
 
 app = Flask(__name__)
@@ -14,4 +17,9 @@ migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+from models import *
+admin = Admin(app)
+admin.add_view(ModelView(Post, db.session))
+admin.add_view(ModelView(Tag, db.session))
 
--- app/config.py
+++ app/config.py
@@ -2,3 +2,4 @@ class Configuration():
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://sblog:sblogDBpasswd@localhost/sblogdb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'Really strong key for my application'
