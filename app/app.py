2a69376f917831e28e34b00ab080375b0b08b2c9
Andrey Belskiy <diksandab@gmail.com>
2021-03-07 21:41:20 +0600

    Added flask_security objects. Updated requirements

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+from flask_security import SQLAlchemyUserDatastore, Security
 
 from config import Configuration
 
@@ -23,3 +24,5 @@ admin = Admin(app)
 admin.add_view(ModelView(Post, db.session))
 admin.add_view(ModelView(Tag, db.session))
 
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
